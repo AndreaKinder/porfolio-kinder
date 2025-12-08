b6f52ecc281e48aeb02802a2800daf104bf2cb3b
Andrea <andvillart@gmail.com>
2025-12-08 17:50:44 +0100

    feat(skills.astro): update project list

--- src/js/tagProjectSelector.js
+++ src/js/tagProjectSelector.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".skill-chip").forEach(chip => {
     chip.addEventListener("click", () => {
-      document.querySelectorAll(".progects-container").forEach(chip => chip.classList.remove("non"));
+      document.querySelectorAll(".projects-container").forEach(chip => chip.classList.remove("non"));
       document.querySelectorAll(".skill-chip").forEach(chip => chip.classList.remove("active-chip"));
       document.querySelectorAll(".project-card").forEach(progect => progect.style.display = "none");
       chip.classList.add("active-chip");
